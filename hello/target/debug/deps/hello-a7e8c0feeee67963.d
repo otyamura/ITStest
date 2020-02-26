b7fa0f3f7cf6b48e0218832201fecc59a1eb97c1
GGBBB2000 <ggbbb@mineo.jp>
2020-02-26 14:58:05 +0900

    素晴らしい言語のプログラムを追加

---
+++ hello/target/debug/deps/hello-a7e8c0feeee67963.d
@@ -0,0 +1,5 @@
+/home/cs17075/ITStest/hello/target/debug/deps/hello-a7e8c0feeee67963: src/main.rs
+
+/home/cs17075/ITStest/hello/target/debug/deps/hello-a7e8c0feeee67963.d: src/main.rs
+
+src/main.rs:
